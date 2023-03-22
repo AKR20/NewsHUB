1c717a4fdf86ac8062f3a709e63a471731c3921e
Akshay Rahul <akshayrahul92@gmail.com>
2023-03-23 03:32:57 +0530

    done

--- components/header.js
+++ components/header.js
@@ -9,9 +9,7 @@ export const Header = () => {
       <div className={styles.main}>
       <header>
         <nav>
-          
             <h2 style={{marginLeft: '80px', marginTop: '30px',marginBottom: '22px', color: 'white', fontSize: '32px'}}>Category</h2>
-          
           <ul>
           <button onClick={() => router.push('/business/1')}>Business</button>
           <button onClick={() => router.push('/sports/1')}>Sports</button>
