c517e2033d94ae8a57c2e72ba5b246304ee7a613
Akshay Rahul <akshayrahul92@gmail.com>
2023-03-23 04:40:45 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Add support for importing modules using absolute paths
+    config.resolve.modules.push(path.resolve('./'));
+
+    // Exclude the `node_modules` folder from being processed by babel
+    config.module.rules.push({
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['next/babel'],
+        },
+      },
+    });
+
+    // Modify the webpack configuration only for the server build
+    if (isServer) {
+      // Add a custom plugin to the webpack configuration
+      config.plugins.push(new MyCustomWebpackPlugin());
+    }
+
+    return config;
+  },
+};
