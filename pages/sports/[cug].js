d1fc88674b08d6393b34f55ce0342838f0ad83b3
Akshay Rahul <akshayrahul92@gmail.com>
2023-03-22 23:43:49 +0530

    first commit

---
+++ pages/sports/[cug].js
@@ -0,0 +1,83 @@
+import { Toolbar } from '@/components/Toolbar';
+import { Header } from '@/components/Header';
+import { SearchBar } from '@/components/SearchBar';
+import styles from '@/styles/Feed.module.css';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
+export const Sports = ({pageNumber, articles}) => {
+    const router = useRouter();
+    return (
+        <div className='page-container'>
+            <Toolbar/>
+            <Header/>
+            <SearchBar/>
+            <div className={styles.heading}><h1> SPORTS NEWS</h1></div>
+        <div className={styles.main} >
+            {articles.map((article, index) => (
+                <div key={index} className={styles.post}>
+                    <h1 onClick={() => (window.location.href = article.url)}>{article.title}</h1>
+                    <p>{article.description}</p>
+                    {!!article.urlToImage && <img src={article.urlToImage}/>}
+                </div>
+            ))}
+        </div>
+        <div className={styles.paginator}>
+            <div 
+            onClick={() => {
+            if(pageNumber>1){
+                router.push(`/sports/${pageNumber - 1}`).then(() => window.scrollTo({ top: 0, behavior: "smooth" }));
+            }}
+            }
+            className={pageNumber === 1 ? styles.disabled : styles.active}>
+                Previous page
+            </div>
+            <div>#{pageNumber}</div> 
+            <div 
+            onClick={() => {
+            if(pageNumber < 5){
+                router.push(`/sports/${pageNumber + 1}`).then(() => window.scrollTo({ top: 0, behavior: "smooth" }));
+            }}
+            }
+            className={pageNumber === 5 ? styles.disabled : styles.active}>
+                Next page
+            </div>
+            </div>
+        </div>
+    );
+  };
+  
+  export const getServerSideProps = async (pageContext) => {
+  const { cug } = pageContext.query;
+  const pageNumber = Number(cug);
+  
+  
+  if (!pageNumber || pageNumber < 1 || pageNumber > 5) {
+    return {
+      props: {
+        articles: [],
+        pageNumber: 1,
+      },
+    };
+  }
+  
+  const apiResponse = await axios.get(
+    `https://newsapi.org/v2/top-headlines?country=in&category=sports&pageSize=5&page=${pageNumber}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
+      },
+    }
+  );
+  
+  const { articles } = apiResponse.data;
+  
+  return {
+    props: {
+      articles,
+      pageNumber,
+    },
+  };
+  };
+
+  export default Sports
